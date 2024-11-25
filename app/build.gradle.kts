4f8f75d21310e83f51a8bf0515a11ffbb623800d
diego <63115070+zarkius@users.noreply.github.com>
2024-11-25 22:18:45 +0100

    commit a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,15 @@
+import org.gradle.api.JavaVersion.VERSION_17
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
+    alias(libs.plugins.google.firebase.crashlytics)
+
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx) // Use the latest version
 }
 
 android {
@@ -9,12 +18,12 @@ android {
 
     defaultConfig {
         applicationId = "com.playconsole.aprendeprogramar2"
-        minSdk = 24
-        targetSdk = 34
+        minSdk = 21
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        signingConfig = signingConfigs.getByName("debug")
     }
 
     buildTypes {
@@ -24,23 +33,35 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = VERSION_17
+        targetCompatibility = VERSION_17
     }
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
+    dependenciesInfo {
+        includeInApk = true
+        includeInBundle = true
+    }
+    buildToolsVersion = "34.0.0"
+    ndkVersion = "28.0.12674087 rc2"
 }
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.firebase.crashlytics)
+    implementation(libs.play.services.ads)
+    implementation(libs.firebase.analytics)
+    implementation(libs.junit)
+    implementation(libs.androidx.espresso.core)
+    implementation(libs.play.services.maps)
+    implementation(libs.play.services.location)
 }
