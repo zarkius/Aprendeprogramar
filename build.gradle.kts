9e4bd763229a86868c22e16550dee92c85480c23
diego <63115070+zarkius@users.noreply.github.com>
2024-11-26 06:21:03 +0100

    commit a

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-
+abstract class SomeTask : DefaultTask() {
+    @TaskAction
+    fun action() {
+        project.copy {
+            from("source")
+            into("destination")
+        }
+    }
+}
